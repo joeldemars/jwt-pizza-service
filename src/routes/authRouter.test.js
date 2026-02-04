976301234347406d07b9069e653ecb9c2ecee139
Joel Demars <joeltdemars@gmail.com>
2026-02-03 17:44:24 -0700

    Restructure authRouter.test.js

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,14 +1,22 @@
 const request = require("supertest");
 const app = require("../service");
 
-const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
+let testUser;
 let testUserAuthToken;
 
 beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  testUser = createTestUserObject();
   const registerRes = await request(app).post("/api/auth").send(testUser);
   testUserAuthToken = registerRes.body.token;
-  expectValidJwt(testUserAuthToken);
+});
+
+test("register", async () => {
+  const registerRes = await request(app)
+    .post("/api/auth")
+    .send(createTestUserObject());
+
+  expect(registerRes.status).toBe(200);
+  expectValidJwt(registerRes.body.token);
 });
 
 test("login", async () => {
@@ -25,4 +33,13 @@ function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(
     /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
   );
+}
+
+function createTestUserObject() {
+  const name = Math.random().toString(36).substring(2, 12);
+  return {
+    name,
+    email: `${name}@test.com`,
+    password: Math.random().toString(36).substring(2, 12),
+  };
 }
