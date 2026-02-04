a3ecfb2e45943e5c8c1cf3c7464969e1e63968f2
Joel Demars <joeltdemars@gmail.com>
2026-02-03 20:15:36 -0700

    Add franchise creation tests

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,12 +1,22 @@
 const request = require("supertest");
 const app = require("../service");
-const { registerTestUser } = require("../testUtils/authUtils");
+const { Role, DB } = require("../database/database");
+const {
+  createTestUserObject,
+  registerTestUser,
+} = require("../testUtils/authUtils");
 
 let testUser;
 let testUserAuthToken;
 
 beforeAll(async () => {
-  ({ testUser, testUserAuthToken } = await registerTestUser());
+  testUser = {
+    ...createTestUserObject(),
+    roles: [{ role: Role.Admin }],
+  };
+  testUser.id = (await DB.addUser(testUser)).id;
+  testUserAuthToken = (await request(app).put("/api/auth").send(testUser)).body
+    .token;
 });
 
 test("list all franchises", async () => {
@@ -17,3 +27,39 @@ test("list all franchises", async () => {
   expect(Array.isArray(listRes.body.franchises)).toBe(true);
   expect(listRes.body.more).toBeDefined();
 });
+
+test("create franchise", async () => {
+  const franchise = {
+    name: createRandomName(),
+    admins: [{ email: testUser.email }],
+  };
+  const createRes = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send(franchise);
+
+  expect(createRes.status).toBe(200);
+  expect(createRes.body.id).toBeDefined();
+  expect(createRes.body.name).toEqual(franchise.name);
+});
+
+test("create franchise fails when user is not admin", async () => {
+  let { testUser, testUserAuthToken } = await registerTestUser();
+  const franchise = {
+    name: createRandomName(),
+    admins: [{ email: testUser.email }],
+  };
+  const createRes = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send(franchise);
+
+  expect(createRes.status).toBe(403);
+  expect(createRes.body).toMatchObject({
+    message: "unable to create a franchise",
+  });
+});
+
+function createRandomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
