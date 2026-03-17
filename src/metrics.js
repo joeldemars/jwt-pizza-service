1501f3cbee39e021dffd68bbe2154f1a0ee61ba2
Joel Demars <joeltdemars@gmail.com>
2026-03-16 18:50:08 -0600

    Create general outline for metrics

---
+++ src/metrics.js
@@ -0,0 +1,149 @@
+const os = require("os");
+const config = require("./config").metrics;
+
+// Metrics stored in memory
+const requests = {};
+const activeUsers = {};
+
+// TODO:
+// HTTP request: method, time
+// Active users:
+// Auth attempts: successful
+// CPU/Memory
+// Pizzas: success, price, latency
+
+// Middleware to track requests
+function requestTracker(req, res, next) {
+  // TODO: handle/record metrics
+  const endpoint = `[${req.method}] ${req.path}`;
+  requests[endpoint] = (requests[endpoint] || 0) + 1;
+  const start = Date.now();
+  res.on('finish', () => {
+    const latency = Date.now() - start;
+  });
+  next();
+}
+
+// This will periodically send metrics to Grafana
+setInterval(() => {
+  // TODO
+  const metrics = [];
+  Object.keys(requests).forEach((endpoint) => {
+    metrics.push(
+      createMetric("requests", requests[endpoint], "1", "sum", "asInt", {
+        endpoint,
+      }),
+    );
+  });
+
+  metrics.push(
+    createMetric(
+      "greetingChange",
+      greetingChangedCount,
+      "1",
+      "sum",
+      "asInt",
+      {},
+    ),
+  );
+
+  sendMetricToGrafana(metrics);
+}, 10000);
+
+function createMetric(
+  metricName,
+  metricValue,
+  metricUnit,
+  metricType,
+  valueType,
+  attributes,
+) {
+  attributes = { ...attributes, source: config.source };
+
+  const metric = {
+    name: metricName,
+    unit: metricUnit,
+    [metricType]: {
+      dataPoints: [
+        {
+          [valueType]: metricValue,
+          timeUnixNano: Date.now() * 1000000,
+          attributes: [],
+        },
+      ],
+    },
+  };
+
+  Object.keys(attributes).forEach((key) => {
+    metric[metricType].dataPoints[0].attributes.push({
+      key: key,
+      value: { stringValue: attributes[key] },
+    });
+  });
+
+  if (metricType === "sum") {
+    metric[metricType].aggregationTemporality =
+      "AGGREGATION_TEMPORALITY_CUMULATIVE";
+    metric[metricType].isMonotonic = true;
+  }
+
+  return metric;
+}
+
+function sendMetricToGrafana(metrics) {
+  const body = {
+    resourceMetrics: [
+      {
+        scopeMetrics: [
+          {
+            metrics,
+          },
+        ],
+      },
+    ],
+  };
+
+  fetch(`${config.endpointUrl}`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      Authorization: `Bearer ${config.accountId}:${config.apiKey}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP status: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.error("Error pushing metrics:", error);
+    });
+}
+
+function getCpuUsagePercentage() {
+  const cpuUsage = os.loadavg()[0] / os.cpus().length;
+  return cpuUsage.toFixed(2) * 100;
+}
+
+function getMemoryUsagePercentage() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsage = (usedMemory / totalMemory) * 100;
+  return memoryUsage.toFixed(2);
+}
+
+function pizzaPurchase(success, latency, price) {
+  // TODO
+}
+
+function authenticationAttempt(success) {
+  // TODO
+}
+
+function userActive(userId) {
+  // TODO
+}
+
+module.exports = { requestTracker };
